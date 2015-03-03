468efc407385aff20840680aaa045791ade3b956
Vitaly Berg <bergusman@gmail.com>
2015-03-03 09:38:00 +0300

    Change index

---
+++ admin/admin.js
@@ -0,0 +1,3 @@
+var express = require('express');
+var admin = express();
+module.exports = admin;
--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 var express = require('express');
-var app = express();
 
+var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-var pg = require('pg');
+var api = require('./api/api.js');
+app.use('/api', api);
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-})
+var admin = require('./admin/admin.js');
+app.use('/admin', admin);
 
 app.listen(app.get('port'));
